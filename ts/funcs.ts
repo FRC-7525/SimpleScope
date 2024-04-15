d39871c4893726457568b412482623e17f750a0a
Nanobot567 <567.nanobot@gmail.com>
2024-04-15 16:03:04 -0500

    add arrow to show robot orientation

--- ts/funcs.ts
+++ ts/funcs.ts
@@ -3,3 +3,9 @@
 export const degreesToRadians = (x: number): number => (x * Math.PI) / 180;
 
 export const clamp = (num: number, min: number, max: number): number => Math.min(Math.max(num, min), max);
+
+export const rotateAroundPoint = (ctx: any, x: number, y: number, rotation: number) => {
+  ctx.translate(x, y);
+  ctx.rotate(degreesToRadians(rotation));
+  ctx.translate(-x, -y);
+}
